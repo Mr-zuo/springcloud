387f4e05cd9b2a89cc36b3e8147b8b1c15cfd0c8
Ron Zhang <ron.zhang@capillarytech.com>
2020-03-28 21:39:05 +0800

    add config-server module

---
+++ microservice-secondkill/src/main/java/com/ron/combat/microservicesecondkill/MicroserviceSecondkillApplication.java
@@ -0,0 +1,13 @@
+package com.ron.combat.microservicesecondkill;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceSecondkillApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroserviceSecondkillApplication.class, args);
+    }
+
+}
